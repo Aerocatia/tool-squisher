696054c654636926c29dc03b7eaa919755cd6ec1
Aerocatia <aerocatia@tutanota.com>
2026-01-10 06:55:14 +1300

    use a more full list of decal bitmaps here

--- src/tag_groups/decal.c
+++ src/tag_groups/decal.c
@@ -9,10 +9,12 @@
 #include "decal.h"
 #include "bitmap.h"
 
-#define NUMBER_OF_RESOURCE_MAP_DECAL_SPRITES 16
+#define NUMBER_OF_RESOURCE_MAP_DECAL_BITMAPS 38
 
-static const char *decal_external_sprite_bitmaps[] = {
+static const char *decal_external_bitmaps[] = {
+    "effects\\decals\\bullet holes\\bitmaps\\dirt pistol",
     "effects\\decals\\bullet holes\\bitmaps\\stone",
+    "effects\\decals\\bullet holes\\bitmaps\\snow",
     "effects\\decals\\bullet holes\\bitmaps\\metal",
     "effects\\decals\\blood splats\\bitmaps\\blood splat",
     "effects\\decals\\bullet holes\\bitmaps\\unbreakable glass",
@@ -21,20 +23,44 @@ static const char *decal_external_sprite_bitmaps[] = {
     "effects\\decals\\blood splats\\bitmaps\\blood splat hunter",
     "effects\\decals\\blood splats\\bitmaps\\blood splat elite",
     "effects\\decals\\bullet holes\\bitmaps\\ice",
-    "effects\\decals\\bullet holes\\bitmaps\\plasma burn" ,
+    "effects\\decals\\bullet holes\\bitmaps\\plasma burn",
     "effects\\decals\\bullet holes\\bitmaps\\plasma burn inner glow",
-    "effects\\decals\\bullet holes\\bitmaps\\plasma burn outer glow" ,
-    "effects\\decals\\blood splats\\bitmaps\\blood splat engineer" ,
+    "effects\\decals\\bullet holes\\bitmaps\\plasma burn outer glow",
+    "effects\\decals\\blood splats\\bitmaps\\blood splat engineer",
     "effects\\decals\\bullet holes\\bitmaps\\plasma burn large",
     "effects\\decals\\bullet holes\\bitmaps\\plasma green inner glow",
+    "levels\\b30\\decals-environment\\bitmaps\\decal fore symb red",
+    "levels\\b30\\decals-environment\\bitmaps\\decal fore symb blue",
+    "levels\\test\\sidewinder\\decals-environment\\bitmaps\\fore_stripe_plate_red",
+    "levels\\test\\sidewinder\\decals-environment\\bitmaps\\fore_stripe_plate_blue",
+    "effects\\decals\\bullet holes\\bitmaps\\impact flame thrower char",
+    "effects\\decals\\vehicle marks\\bitmaps\\snow treadmarks",
+    "levels\\b30\\decals-environment\\bitmaps\\decal fore symb blue b",
+    "levels\\b30\\decals-environment\\bitmaps\\decal fore symb red b",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\1_blue",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\2_red",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\5_red",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\1_red",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\2_blue",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\3_blue",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\3_red",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\4_blue",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\4_red",
+    "levels\\test\\boardingaction\\decals-environment\\bitmaps\\5_blue",
+    "levels\\a10\\decals-environment\\bitmaps\\decal danger biohazard",
+    "levels\\a10\\decals-environment\\bitmaps\\decal danger electric",
     "effects\\decals\\blood splats\\bitmaps\\glowingdrip splat"
 };
-static_assert(sizeof(decal_external_sprite_bitmaps) / sizeof(char *) == NUMBER_OF_RESOURCE_MAP_DECAL_SPRITES);
+static_assert(sizeof(decal_external_bitmaps) / sizeof(char *) == NUMBER_OF_RESOURCE_MAP_DECAL_BITMAPS);
 
-static const float decal_external_sprite_bitmap_extents[] = {
-    33.0f, 23.0f, 40.0f, 34.0f, 38.0f, 34.0f, 34.0f, 36.0f, 64.0f, 9.0f, 13.0f, 34.0f, 38.0f, 10.0f, 13.0f, 34.0f
+static const float decal_external_bitmap_extents[] = {
+    0.0f,  33.0f, 0.0f,  23.0f, 40.0f, 34.0f, 38.0f, 34.0f,
+    34.0f, 36.0f, 64.0f, 9.0f,  13.0f, 34.0f, 38.0f, 10.0f,
+    13.0f, 0.0f,  0.0f,  0.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+    0.0f,  0.0f,  0.0f,  0.0f,  0.0f,  0.0f,  0.0f,  0.0f,
+    0.0f,  0.0f,  0.0f,  0.0f,  0.0f,  34.0f
 };
-static_assert(sizeof(decal_external_sprite_bitmap_extents) / sizeof(float) == NUMBER_OF_RESOURCE_MAP_DECAL_SPRITES);
+static_assert(sizeof(decal_external_bitmap_extents) / sizeof(float) == NUMBER_OF_RESOURCE_MAP_DECAL_BITMAPS);
 
 bool decal_postprocess(TagID tag, struct tag_data_instance *tag_data) {
     struct decal *decal = tag_get(tag, TAG_FOURCC_DECAL, tag_data);
@@ -63,16 +89,16 @@ bool decal_postprocess(TagID tag, struct tag_data_instance *tag_data) {
     // Yes this does mean you go to the shadow realm if the bitmaps.map contains a completely different custom sprite, but this
     // was always going to be a problem. Ideally the game would re-calculate this on map load when loading external bitmap tags.
     if(tag_is_external(map, tag_data)) {
-        for(size_t i = 0; i < NUMBER_OF_RESOURCE_MAP_DECAL_SPRITES; i++) {
-            if(strcmp(tag_path_get(map, tag_data), decal_external_sprite_bitmaps[i]) == 0) {
-                decal->runtime_maximum_sprite_extent = decal_external_sprite_bitmap_extents[i];
+        for(size_t i = 0; i < NUMBER_OF_RESOURCE_MAP_DECAL_BITMAPS; i++) {
+            if(strcmp(tag_path_get(map, tag_data), decal_external_bitmaps[i]) == 0) {
+                decal->runtime_maximum_sprite_extent = decal_external_bitmap_extents[i];
                 return true;
             }
         }
 
-        // non-sprites are zero
-        decal->runtime_maximum_sprite_extent = 0.0f;
-        return true;
+        fprintf(stderr,"external bitmap \"%s.%s\" does not map to a known decal bitmap and can not be processed by this tool\n",
+        tag_path_get(map, tag_data), tag_fourcc_to_extension(TAG_FOURCC_BITMAP));
+        return false;
     }
 
     struct bitmap *bitmap_group = tag_get(map, TAG_FOURCC_BITMAP, tag_data);
